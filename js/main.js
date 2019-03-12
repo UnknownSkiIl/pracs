a36ef9db10e4a85ec7035cdad5e3bcc6dc02b5a8
Sergey Tkachenko <hukl856@gmail.com>
2019-03-12 15:17:15 +0200

    was added the ezAjaxForm sender script and some changes to ezPopUp

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     //Timer
 
-    let deadline = '2019-03-10';
+    let deadline = '2019-03-14';
 
     function getTimeRamaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -110,8 +110,9 @@ window.addEventListener('DOMContentLoaded', function () {
 
     //Modal 
 
-    function EzPopUp(btn) {
-        let more = document.querySelector(btn),
+    function EzPopUp(btn, num = 0) {
+        let mores = document.querySelectorAll(btn),
+            more = mores[num],
             overlay = document.querySelector('.overlay'),
             close = document.querySelector('.popup-close');
 
@@ -129,5 +130,63 @@ window.addEventListener('DOMContentLoaded', function () {
     };
 
     EzPopUp('.more');
-    EzPopUp('.description-btn');
+    EzPopUp('.description-btn', 0);
+    EzPopUp('.description-btn', 1);
+    EzPopUp('.description-btn', 2);
+    EzPopUp('.description-btn', 3);
+
+    //Form 
+    function ezAjaxForm(selector) {
+
+        let messege = {
+            loading: 'Загрузка...',
+            success: 'Спасибо! Скоро мы вам ответим!',
+            failure: 'Что-то пошло не так :('
+        };
+
+        let form = document.querySelector(selector),
+            input = form.getElementsByTagName('input'),
+            statusMessege = document.createElement('div');
+        
+        statusMessege.classList.add('status');
+
+        form.addEventListener('submit', function(event) {
+            event.preventDefault(); 
+            form.appendChild(statusMessege);
+
+            let request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            // request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+            let formData = new FormData(form);
+            
+            // Если отправляем данные в JSON
+            let obj = {};
+            formData.forEach(function(value,key) {
+                obj[key] = value;
+            });
+            let json = JSON.stringify(obj);
+
+            request.send(json);
+            // request.send(FormData);
+
+            request.addEventListener('readystatechange', function() {
+                if (request.readyState < 4) {
+                    statusMessege.innerHTML = messege.loading;
+                } else if(request.readyState === 4 && request.status == 200) {
+                    statusMessege.innerHTML = messege.success;
+                } else {
+                    statusMessege.innerHTML = messege.failure;
+                }
+            });
+
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = '';
+            }
+        });
+    }
+
+    ezAjaxForm('.main-form');
+    ezAjaxForm('#form');
 });
